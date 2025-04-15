e0a2060b9128acb48ac033a3cecaab7d0562df24
Gareth Powell <gareth@ziniki.org>
2025-04-15 13:26:04 -0400

    Extracted three functions

--- golden/internal/runner/compare.go
+++ golden/internal/runner/compare.go
@@ -3,6 +3,7 @@ package runner
 import (
 	"path/filepath"
 
+	"ziniki.org/deployer/deployer/pkg/deployer"
 	"ziniki.org/deployer/deployer/pkg/utils"
 	"ziniki.org/deployer/golden/internal/errors"
 )
@@ -33,44 +34,14 @@ func (gc *goldenComparator) compareAll() {
 func (gc *goldenComparator) compareDirectory(golden, gen string, copyNewFiles bool) {
 	base := filepath.Base(golden)
 	eh := gc.tracker.ErrorHandlerFor("golden-" + base)
-	eh.Writef("comparing %s to %s\n", golden, gen)
-	goldenFiles, err1 := utils.FindFiles(golden, "")
-	genFiles, err2 := utils.FindFiles(gen, "")
-	if err1 != nil && err2 != nil {
-		// it's "safe" to assume it's an empty case
-		return
-	}
-	if err1 != nil {
-		// OK, it should be there.  Create it and we'll copy the files in
-		utils.EnsureDir(golden)
-	}
-	if err2 != nil {
-		// Presumably if there is a golden dir, there should be a gen dir
-		eh.Writef("error collecting generated files from %s\n", gen)
+	goldenFiles, genFiles, err := gc.prepare(eh, golden, gen)
+	if err != nil {
 		return
 	}
 
 	// Go through the golden files, comparing to the generated ones
-	genmap := make(map[string]int)
-	for k, g := range genFiles {
-		genmap[g] = k + 1
-	}
-	failed := false
-	for _, f := range goldenFiles {
-		if genmap[f] != 0 {
-			if !utils.CompareFiles(filepath.Join(gen, f), filepath.Join(golden, f)) {
-				eh.Writef("generated file %s did not match golden file\n", f)
-				eh.Fail()
-			}
-			delete(genmap, f)
-		} else { // if there is no generated file, complain: that's a failure
-			eh.Writef("there is no gen file for %s\n", f)
-			if !failed {
-				eh.Fail()
-				failed = true
-			}
-		}
-	}
+	genmap := gc.catalogGenned(genFiles)
+	gc.traverseGolden(eh, genmap, golden, gen, goldenFiles)
 
 	// If there are any generated files which don't have golden files, let the user know and copy them
 	if len(genmap) > 0 {
@@ -84,6 +55,54 @@ func (gc *goldenComparator) compareDirectory(golden, gen string, copyNewFiles bo
 			for _, f := range genFiles {
 				eh.Writef("there is no golden file for generated %s\n", f)
 				eh.Fail()
+			}
+		}
+	}
+}
+
+func (gc *goldenComparator) prepare(eh deployer.ErrorHandler, golden, gen string) ([]string, []string, error) {
+	eh.Writef("comparing %s to %s\n", golden, gen)
+	goldenFiles, err1 := utils.FindFiles(golden, "")
+	genFiles, err2 := utils.FindFiles(gen, "")
+	if err1 != nil && err2 != nil {
+		// it's "safe" to assume it's an empty case
+		return goldenFiles, genFiles, err1
+	}
+	if err1 != nil {
+		// OK, it should be there.  Create it and we'll copy the files in
+		utils.EnsureDir(golden)
+	}
+	if err2 != nil {
+		// Presumably if there is a golden dir, there should be a gen dir
+		eh.Writef("error collecting generated files from %s\n", gen)
+		eh.Fail()
+		return goldenFiles, genFiles, err2
+	}
+	return goldenFiles, genFiles, nil
+}
+
+func (gc *goldenComparator) catalogGenned(genFiles []string) map[string]int {
+	genmap := make(map[string]int)
+	for k, g := range genFiles {
+		genmap[g] = k + 1
+	}
+	return genmap
+}
+
+func (gc *goldenComparator) traverseGolden(eh deployer.ErrorHandler, genmap map[string]int, golden, gen string, goldenFiles []string) {
+	failed := false
+	for _, f := range goldenFiles {
+		if genmap[f] != 0 {
+			if !utils.CompareFiles(filepath.Join(gen, f), filepath.Join(golden, f)) {
+				eh.Writef("generated file %s did not match golden file\n", f)
+				eh.Fail()
+			}
+			delete(genmap, f)
+		} else { // if there is no generated file, complain: that's a failure
+			eh.Writef("there is no gen file for %s\n", f)
+			if !failed {
+				eh.Fail()
+				failed = true
 			}
 		}
 	}
