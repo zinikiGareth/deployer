57e8b76d33768f358c1532b54dae8aa7c7754ffc
Gareth Powell <gareth@ziniki.org>
2025-05-01 14:59:50 +0100

    WIP: starting on more serious parsing

--- deployer/pkg/interpreters/propertiesinnerscope.go
+++ deployer/pkg/interpreters/propertiesinnerscope.go
@@ -15,12 +15,40 @@ type propertiesInnerScope struct {
 }
 
 func (pis *propertiesInnerScope) HaveTokens(reporter errors.ErrorRepI, tokens []pluggable.Token) pluggable.Interpreter {
-	if len(tokens) != 3 {
-		reporter.Report(0, "invalid property yada yada")
+	// TODO: the left hand side must be an identifier
+	// then it must be "<-"
+	// the rest of the tokens must be an expression
+	if len(tokens) < 3 {
+		reporter.Report(0, "<prop> <- <expr>")
 		return DisallowInnerScope()
 	}
-	pis.parent.AddProperty(reporter, tokens[0].(pluggable.Identifier), tokens[2])
+
+	prop, ok := tokens[0].(pluggable.Identifier)
+	if !ok {
+		panic("nice error please")
+	}
+
+	op, ok := tokens[1].(pluggable.Operator)
+	if !ok {
+		panic("nice error please")
+	} else if !op.Is("<-") {
+		panic("not <-")
+	}
+
+	expr, ok := parseExpr(tokens[2:])
+	if !ok {
+		return IgnoreInnerScope()
+	}
+	pis.parent.AddProperty(reporter, prop, expr)
 	return DisallowInnerScope()
+}
+
+// TODO: this should be elsewhere
+func parseExpr(tokens []pluggable.Token) (pluggable.Locatable, bool) { // TODO: should be expr
+	if len(tokens) == 1 {
+		return tokens[0], true
+	}
+	panic("this needs to be implemented")
 }
 
 func (b *propertiesInnerScope) Completed(reporter errors.ErrorRepI) {
