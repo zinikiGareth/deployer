c0431c21d53cdc6c9a5383da0a62ac88fac733be
Gareth Powell <gareth@ziniki.org>
2025-06-25 13:52:53 +0100

    Added test of attaching policies to buckets

--- testmod/internal/testS3/bucket_creator.go
+++ testmod/internal/testS3/bucket_creator.go
@@ -12,11 +12,12 @@ import (
 type bucketCreator struct {
 	tools *corebottom.Tools
 
-	loc   *errorsink.Location
-	name  string
-	props map[driverbottom.Identifier]driverbottom.Expr
-	env   *TestAwsEnv
-	cloud *BucketCloud
+	loc    *errorsink.Location
+	name   string
+	props  map[driverbottom.Identifier]driverbottom.Expr
+	env    *TestAwsEnv
+	cloud  *BucketCloud
+	policy corebottom.PolicyDocument
 }
 
 func (b *bucketCreator) Loc() *errorsink.Location {
@@ -95,4 +96,18 @@ func (eb *bucketCreator) String() string {
 	return fmt.Sprintf("EnsureBucket[%s:%s]", eb.env.Region, eb.name)
 }
 
+// TODO: this should be on the model
+func (eb *bucketCreator) Attach(doc corebottom.PolicyDocument) {
+	// TODO: this should copy the model
+	eb.policy = doc
+	tmp := eb.tools.Recall.ObtainDriver("testhelpers.TestStepLogger")
+	testLogger, ok := tmp.(testhelpers.TestStepLogger)
+	if !ok {
+		panic("could not cast logger to TestStepLogger")
+	}
+
+	testLogger.Log("we need to attach policy with %d items to bucket %s\n", len(doc.Items()), eb.name)
+}
+
 var _ corebottom.Ensurable = &bucketCreator{}
+var _ corebottom.PolicyAttacher = &bucketCreator{} // should be model
