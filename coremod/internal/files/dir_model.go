cb7c3902b1ce5e33266b3646497194a982d72a86
Gareth Powell <gareth@ziniki.org>
2025-06-27 09:06:25 +0100

    Tidied up dir model

--- coremod/internal/files/dir_model.go
+++ coremod/internal/files/dir_model.go
@@ -42,49 +42,58 @@ func (d *DirModel) DumpTo(iw driverbottom.IndentWriter) {
 }
 
 func (d *DirModel) ObtainPourer() {
-	var err error
+	if d.pourer != nil {
+		return
+	}
 	for _, v := range d.paths {
 		if d.pourer == nil {
-			p, ok := v.(*DirectoryPourer)
-			if ok {
+			switch p := v.(type) {
+			case *DirectoryPourer:
 				d.pourer = p
-			} else {
-				tmp, ok := v.(fmt.Stringer)
-				if ok {
-					v = tmp.String()
-				}
-				s, ok := v.(string)
-				if ok {
-					if filepath.IsAbs(s) {
-						d.pourer, err = NewDirectoryPourer(s)
-						if err != nil {
-							panic(err)
-						}
-					} else {
-						panic(fmt.Sprintf("cannot use non-abs path here: %v\n", v))
-					}
-				} else {
-					panic(fmt.Sprintf("cannot handle base path %T\n", v))
-				}
+			case fmt.Stringer:
+				d.pourer = handleBaseString(p.String())
+			case string:
+				d.pourer = handleBaseString(p)
+			default:
+				panic(fmt.Sprintf("cannot handle base path %T\n", v))
 			}
 		} else {
-			s, ok := v.(string)
-			if ok {
-				if !filepath.IsAbs(s) {
-					d.pourer, err = d.pourer.Relative(s)
-					if err != nil {
-						panic(err)
-					}
-				} else {
-					panic(fmt.Sprintf("cannot use abs path here: %v\n", v))
-				}
-			} else {
+			switch p := v.(type) {
+			case fmt.Stringer:
+				d.pourer = handleNestedString(d.pourer, p.String())
+			case string:
+				d.pourer = handleNestedString(d.pourer, p)
+			default:
 				panic(fmt.Sprintf("cannot handle nested path %T\n", v))
 			}
 		}
 	}
 }
 
+func handleBaseString(s string) *DirectoryPourer {
+	if filepath.IsAbs(s) {
+		pourer, err := NewDirectoryPourer(s)
+		if err != nil {
+			panic(err)
+		}
+		return pourer
+	} else {
+		panic(fmt.Sprintf("cannot use non-abs path here: %s\n", s))
+	}
+}
+
+func handleNestedString(dp *DirectoryPourer, s string) *DirectoryPourer {
+	if !filepath.IsAbs(s) {
+		pourer, err := dp.Relative(s)
+		if err != nil {
+			panic(err)
+		}
+		return pourer
+	} else {
+		panic(fmt.Sprintf("cannot use abs path here: %s\n", s))
+	}
+
+}
 func (dp *DirModel) PourAll(into corebottom.FileDest) {
 	dp.ObtainPourer()
 	dp.pourer.PourAll(into)
@@ -96,7 +105,11 @@ func (dp *DirModel) PourOut(name string, into corebottom.FileDest) {
 }
 
 func NewDirModel(loc *errorsink.Location, paths []any) *DirModel {
-	return &DirModel{loc: loc, paths: paths}
+	ret := &DirModel{loc: loc, paths: paths}
+	// TODO: this needs a little bit of care, because there could be issues with things not being resolved yet
+	// We need to trap those
+	ret.ObtainPourer() // check that the paths exist as early as possible
+	return ret
 }
 
 var _ driverbottom.Describable = &DirModel{}
