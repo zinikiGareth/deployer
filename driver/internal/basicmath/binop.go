c31d7f7706c1d9f124645a317a62ef6e7b7d6a76
Gareth Powell <gareth@ziniki.org>
2025-07-18 12:41:48 -0400

    refactored binop

---
+++ driver/internal/basicmath/binop.go
@@ -0,0 +1,35 @@
+package basicmath
+
+import (
+	"fmt"
+
+	"ziniki.org/deployer/driver/pkg/driverbottom"
+	"ziniki.org/deployer/driver/pkg/errorsink"
+)
+
+type binop struct {
+	driverbottom.Locatable
+	opname string
+	lhs    driverbottom.Expr
+	rhs    driverbottom.Expr
+}
+
+func (m *binop) Loc() *errorsink.Location {
+	panic("unimplemented")
+}
+
+func (m *binop) ShortDescription() string {
+	return fmt.Sprintf("%s [%s,%s]", m.opname, m.lhs.ShortDescription(), m.rhs.ShortDescription())
+}
+
+func (m *binop) DumpTo(to driverbottom.IndentWriter) {
+	panic("unimplemented")
+}
+
+func (m *binop) String() string {
+	panic("unimplemented")
+}
+
+func (m *binop) Resolve(r driverbottom.Resolver) {
+	panic("unimplemented")
+}
--- driver/internal/basicmath/multiply.go
+++ driver/internal/basicmath/multiply.go
@@ -1,41 +1,11 @@
 package basicmath
 
 import (
-	"fmt"
-
 	"ziniki.org/deployer/driver/pkg/driverbottom"
-	"ziniki.org/deployer/driver/pkg/errorsink"
 )
-
-type binop struct {
-	driverbottom.Locatable
-	opname string
-	lhs    driverbottom.Expr
-	rhs    driverbottom.Expr
-}
 
 type multiplyExpr struct {
 	binop
-}
-
-func (m *binop) Loc() *errorsink.Location {
-	panic("unimplemented")
-}
-
-func (m *binop) ShortDescription() string {
-	return fmt.Sprintf("%s [%s,%s]", m.opname, m.lhs.ShortDescription(), m.rhs.ShortDescription())
-}
-
-func (m *binop) DumpTo(to driverbottom.IndentWriter) {
-	panic("unimplemented")
-}
-
-func (m *binop) String() string {
-	panic("unimplemented")
-}
-
-func (m *binop) Resolve(r driverbottom.Resolver) {
-	panic("unimplemented")
 }
 
 func (m *multiplyExpr) Eval(s driverbottom.RuntimeStorage) any {
