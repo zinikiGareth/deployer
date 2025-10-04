10e236de1a413feb40286161e948488c4b757af7
Gareth Powell <gareth@ziniki.org>
2025-10-04 21:29:33 +0100

    Added a couple more unit tests

--- driver/internal/parser/exprs/invoke_prec_test.go
+++ driver/internal/parser/exprs/invoke_prec_test.go
@@ -3,6 +3,7 @@ package exprs_test
 import (
 	"testing"
 
+	"ziniki.org/deployer/driver/internal/basicmath"
 	"ziniki.org/deployer/driver/internal/methods"
 	"ziniki.org/deployer/driver/internal/parser/lexicator"
 	"ziniki.org/deployer/driver/pkg/driverbottom"
@@ -20,6 +21,38 @@ func TestParseMultipleWithNoParensGivesError(t *testing.T) {
 	x := lexicator.NewIdentifierToken(lineloc, 0, "x")
 	y := lexicator.NewIdentifierToken(lineloc, 0, "y")
 	_, ok := p.Parse(nil, []driverbottom.Token{f, x, arrow, y})
+	if ok {
+		t.Fatalf("parsing incorrectly succeeded")
+	}
+}
+
+func TestParseInsideFnWithNoParensGivesError(t *testing.T) {
+	p, h := makeParser(t)
+	lineloc.Text = "f + ->y"
+	h.Sink.Expect(1, 1, 26, lineloc.Text, "no expr to left of ->")
+
+	recall.things["+"] = basicmath.MakeAddFunc(h.Tools)
+	recall.things["->"] = methods.MakeInvokeFunc(h.Tools)
+
+	f := lexicator.NewIdentifierToken(lineloc, 0, "f")
+	plus := lexicator.NewOperatorToken(lineloc, 0, "+")
+	y := lexicator.NewIdentifierToken(lineloc, 0, "y")
+	_, ok := p.Parse(nil, []driverbottom.Token{f, plus, arrow, y})
+	if ok {
+		t.Fatalf("parsing incorrectly succeeded")
+	}
+}
+
+
+func TestNoObjectOnArrowGivesError(t *testing.T) {
+	p, h := makeParser(t)
+	lineloc.Text = "->y"
+	h.Sink.Expect(1, 1, 26, lineloc.Text, "no expr to left of ->")
+
+	recall.things["->"] = methods.MakeInvokeFunc(h.Tools)
+
+	y := lexicator.NewIdentifierToken(lineloc, 0, "y")
+	_, ok := p.Parse(nil, []driverbottom.Token{arrow, y})
 	if ok {
 		t.Fatalf("parsing incorrectly succeeded")
 	}
